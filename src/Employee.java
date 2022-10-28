93411b82746dbd0b07f32ab1a77d65ca2d840bae
reksa ariansyah <reksa.ariansyah@gmail.com>
2022-10-28 19:21:50 +0700

    add employee class

---
+++ src/Employee.java
@@ -0,0 +1,9 @@
+public class Employee {
+    private String nama;
+    private String alamat;
+
+    public Employee(String nama, String alamat) {
+        this.nama = nama;
+        this.alamat = alamat;
+    }
+}
